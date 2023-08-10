50ff889b13e96ae0f87dca1bc7dfa513da84cc6e
Tahir <cs.tahirhussain@gmail.com>
2023-08-10 14:40:13 +0500

    handling aspect ratio and screen resize

--- index.js
+++ index.js
@@ -1,19 +1,24 @@
 let video = null;
 let canvas = null;
 let context = null;
+let scalar = 0.8;
+size = { x: 0, y: 0, width: 0, height: 0 };
 
 async function main() {
   canvas = document.getElementById("canvas");
   context = canvas.getContext("2d");
-  canvas.width = window.innerWidth;
-  canvas.height = window.innerHeight;
+
   try {
     let signal = await navigator.mediaDevices.getUserMedia({ video: true });
 
     video = document.createElement("video");
     video.srcObject = signal;
     video.play();
-    video.onloadeddata = () => updateCanvas();
+    video.onloadeddata = () => {
+      handleResize();
+      window.addEventListener("resize", handleResize);
+      updateCanvas();
+    };
   } catch (error) {
     alert(`camera errror ${error}`);
   }
@@ -21,5 +26,27 @@ async function main() {
 
 function updateCanvas() {
   context.drawImage(video, 0, 0);
-  window.requestAnimationFrame(updateCanvas);
+  window.requestAnimationFrame(
+    updateCanvas,
+    size.x,
+    size.y,
+    size.width,
+    size.height
+  );
+}
+
+function handleResize() {
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+  let resizer =
+    scalar *
+    Math.min(
+      window.innerHeight / video.videoWidth,
+      window.innerHeight / video.videoHeight
+    );
+
+  size.width = resizer * video.videoWidth;
+  size.height = resizer * video.videoHeight;
+  size.x = window.innerWidth / 2 - size.width / 2;
+  size.y = window.innerHeight / 2 - size.height / 2;
 }
